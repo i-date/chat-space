9c3bcef7e11f8c4fab9c1e0e5836bc75d6d2d7ba
N. Idate <in.my.challenge@gmail.com>
2018-12-02 13:36:20 +0900

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :members
+  has_many :users, through: :members
   validates :name, presence: true
 end
