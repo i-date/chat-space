20ee9db0a639befd2c16ea1b52813fbf4c1afb0c
N. Idate <in.my.challenge@gmail.com>
2018-12-05 11:25:25 +0900

    modify image path in spec/factories/messages

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,10 @@ FactoryGirl.define do
 
   factory :message do
     body  Faker::Lorem.sentence
-    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon/jpg'))
+    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/fixtures/icon.jpg'))
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+    user
+    group
   end
 
 end
