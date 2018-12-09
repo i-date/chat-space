58fe15eeb0c8804f56739d15c8dc7f494990dda3
N. Idate <in.my.challenge@gmail.com>
2018-12-09 14:45:20 +0900

    add auto scrolling to javascripts/message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,9 @@
+$(window).load(function () {
+  $(".chat__body").animate({
+    scrollTop: $(".chat__body")[0].scrollHeight
+  }, 0);
+});
+
 $(function(){
   function timeFormat(data) {
     var formatTime = data.replace(/T/, " ").split(".", 1);
