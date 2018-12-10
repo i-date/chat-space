754d1caa92b46629a2282b760379e198b967c3e2
N. Idate <in.my.challenge@gmail.com>
2018-12-10 22:02:53 +0900

    Improve readability of javascripts/message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function () {
   function buildHTML(message) {
-    var html = `<li class="chat__body__list">
+    var html = `<li class="chat__body__list" id="message__${message.message_id}">
                   <span class="chat__body__list__user-name">${ message.user_name }</span>
                   <span class="chat__body__list__creation-time">${ message.created_at }</span>
                   <div class="chat__body__list__message">`
@@ -11,39 +11,34 @@ $(function () {
     return html;
   }
 
-  function buildGroupNewMessage(newMessage) {
-    var groupNewMessage;
-    if (newMessage.length == 0) {
-      return groupNewMessage = "画像が投稿されています";
-    } else {
-      return groupNewMessage = newMessage;
-    }
-  }
-
   $("#message-form").submit(function (e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url      = $(this).attr("action");
+    var url = $(this).attr("action");
+
     $.ajax({
-      url        : url,
-      type       : "POST",
-      data       : formData,
-      dataType   : 'json',
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
       processData: false,
       contentType: false
     })
+
     .done(function (data) {
       var html = buildHTML(data);
-      var groupNewMessage = buildGroupNewMessage(data.body);
+      var groupNewMessage = data.body.length ? data.body : "画像が投稿されています";
       $("#group__" + data.group_id + "> .chat-nav__group__list__new-message").html(groupNewMessage);
       $(".chat__body").append(html);
       $(".chat__body").animate({
         scrollTop: $(".chat__body")[0].scrollHeight
       }, "fast");
     })
+
     .fail(function () {
-      alert('error');
+      alert("error");
     })
+
     .always(function () {
       $(".chat__footer__submit").removeAttr("disabled");
       $("#message-form")[0].reset();
