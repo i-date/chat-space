d784094418f045999b4f22b2e216280c3596fde4
N. Idate <in.my.challenge@gmail.com>
2018-12-10 13:05:36 +0900

    modify scrolling.js

--- app/assets/javascripts/scrolling.js
+++ app/assets/javascripts/scrolling.js
@@ -1,5 +1,8 @@
-$(window).load(function(){
-  $(".chat__body").animate({
-    scrollTop: $(".chat__body")[0].scrollHeight
-  }, 0);
+$(window).load(function () {
+  var chatBody = $(".chat__body");
+  if (chatBody.length) {
+    $(".chat__body").animate({
+      scrollTop: $(".chat__body")[0].scrollHeight
+    }, 0);
+  }
 })
