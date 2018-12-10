50ccdeb79ec035e97b6caae7a7c8e1eb3c45def5
N. Idate <in.my.challenge@gmail.com>
2018-12-10 10:54:20 +0900

    add index action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where("name LIKE(?) && id != #{current_user.id}", "%#{params[:members]}%").limit(20)
+    respond_to do |format|
+     format.json
+    end
+  end
+
   def edit
   end
 
