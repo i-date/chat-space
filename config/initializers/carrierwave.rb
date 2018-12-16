b21ddb697935b716bc9f23e3aa840cd8d73ee1fa
N. Idate <in.my.challenge@gmail.com>
2018-12-16 17:26:33 +0900

    rename Bucket form upload-photo.data to upload-image-data

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'upload-photo.data'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-photo.data'
+  config.fog_directory  = 'upload-image-data'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-image-data'
 end
